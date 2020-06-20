57f3548e7fa27fa6c771d66786ccbab6537d7b3e
user.email <875691208@qq.com>
2020-06-20 01:53:40 -0500

    add files

---
+++ i3/,/touchpad_toggle.sh
@@ -0,0 +1,28 @@
+#! /bin/bash 
+
+# get the touchpad ID
+# put your device name below. 
+# you can use 
+	# $: xinput list to check you touchpad
+
+ID=15
+
+# get status 
+STATUS=$(xinput list-props $ID | grep "Device Enabled" | awk '{ print $4 }')
+
+# get the tap status and property ID 
+# TEMP=$(input list-props $ID | grep --max-count=1 "Tapping Enabled")
+
+case $1 in 
+	toggle_touchpad)
+		if [ $STATUS -ne 0 ]
+		then 
+			xinput --disable $ID 
+		else 
+			xinput --enable $ID 
+		fi
+	;;
+	start_touchpad)
+		xinput --disable $ID 
+	;;
+esac
